262a3dfba2e8678f774e2d2ea168741f5a3c4abc
MusheerAhmed <musheerjamadar1024@gmail.com>
2021-09-02 13:42:42 +0530

    added c

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const mysql = require('mysql');
 
 // Connection Pool
@@ -45,12 +46,32 @@ exports.find = (req, res) => {
             } else {
                 console.log(err);
             }
-        })
+        });
     });
 }
 
-//Add new user
+//render add user
 exports.form = (req, res) => {
     res.render('add-user');
+}
+
+//Add new user
+exports.create = (req, res) => {
+    const { first_name, last_name, email, phone, comments } = req.body;
+    // res.render('add-user');
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        console.log("Connected with CID " + connection.threadId);
+
+        connection.query(`insert into users set first_name = ?, last_name = ?, email = ?, phone = ?, comments = ?`
+            , [first_name, last_name, email, phone, comments], (err, rows) => {
+                connection.release();
+                if (!err) {
+                    res.render('add-user');
+                }
+            });
+
+
+    })
 
 }
